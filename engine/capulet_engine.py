ba9a35c581965ca69269549225c3836c7ffbde8b
Babajide Sanusi <sansbabshabs@gmail.com>
2022-06-09 02:15:40 -0400

    Minor Fixes

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,7 +1,7 @@
 from engine.engine import Engine
 
 
-class CapuletEngine:
+class CapuletEngine(Engine):
     def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
